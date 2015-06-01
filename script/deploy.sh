a3b9780759ba9f9e28ccf6bbcab224653b843c05
Robin Clowers <robin.clowers@gmail.com>
2015-06-01 12:17:10 -0500

    Exclude all dot files / directories

--- script/deploy.sh
+++ script/deploy.sh
@@ -2,4 +2,4 @@
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-rsync -arv --delete --exclude "script" "$DIR/../" root@107.170.241.16:/srv/robinclowers.com/
+rsync -arv --delete --exclude "script" --exclude ".*" --exclude ".*/" "$DIR/../" root@107.170.241.16:/srv/robinclowers.com/
