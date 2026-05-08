c6f2e9ab3bdb5384acc6523d783829f2d0084a2a
Robin Clowers <robin.clowers@gmail.com>
2026-05-07 21:55:59 -0700

    Remove scripts

--- script/deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-
-rsync -arv --delete --exclude "script" --exclude ".*" --exclude ".*/" "$DIR/../" root@107.170.241.16:/srv/robinclowers.com/
--- script/server.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-ruby -rwebrick -e'WEBrick::HTTPServer.new(:Port => 8000, :DocumentRoot => Dir.pwd).start'
